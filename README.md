c8df0ffd6d69df6a842157fa560b1bf1bc6fbcf3
Piyush Bhatt <piyush.bhatt@ad.infosys.com>
2019-11-26 21:48:46 +0530

    Version 1.0

---
+++ README.md
@@ -0,0 +1,97 @@
+# create-component
+
+create-component is a CLI generator for ReactJS components based on specified name and options.
+
+## Content
+
+* [Installation](#installation)
+* [Usage](#usage)
+  * [Functional Component](#functional-component)
+  * [Class Based Component](#class-based-component)
+  * [Options](#options)
+* [Examples](#examples)
+* [Contributing](#contributing)
+* [License](#license)
+
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install
+
+```bash
+npm install -g create-component
+```
+
+## Usage
+
+Make sure to execute the commands from your app root directory
+
+### Functional Component
+```bash
+create-component <component-name> -f
+```
+   or
+```bash
+cc <component-name> --function
+```
+
+### Class Based Component
+```bash
+create-component <component-name> -c
+```
+   or
+```bash
+cc <component-name> --class
+```
+
+
+### Options
+
+Following options can be used optionally,
+
+**--css** : add a CSS module for the generated component, e.g.,
+```bash
+create-component <component-name> --class --css
+```
+
+**-s** or **--state** : add state to the generated component, e.g.,
+```bash
+create-component <component-name> --class --state
+```
+
+## Examples
+
+* Sample component (functional component with state and css):
+
+    ```bash
+    create-component sample --function --css --state
+
+    CREATED <app-root-dir>\src\sample\sample.css
+    CREATED <app-root-dir>\src\sample\sample.js
+    CREATED <app-root-dir>\src\sample\index.js
+    ```
+* Sample component (class based component without state but with css):
+
+    ```bash
+    create-component sample --class --css
+
+    CREATED <app-root-dir>\src\sample\sample.css
+    CREATED <app-root-dir>\src\sample\sample.js
+    CREATED <app-root-dir>\src\sample\index.js
+    ```
+* Sample component (functional component in nested directory without state and css):
+
+    ```bash
+    create-component components/sample --function
+
+    CREATED <app-root-dir>\src\components\sample\sample.js
+    CREATED <app-root-dir>\src\components\sample\index.js
+    ```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[ISC](https://opensource.org/licenses/ISC)
---
+++ cli.js
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+'use strict';
+
+let componentName;
+const path = require('path');
+const fs = require('fs-extra');
+const program = require('commander')
+    .version(require('./package.json').version)
+    .arguments('<name> [errorEntry]')
+    .action((name, errorEntry) => {
+        componentName = name;
+        errorEntry = errorEntry;
+        if (errorEntry !== undefined) {
+            console.error('ERROR: Component name cannot contain spaces!');
+            process.exit(1);
+        }
+    })
+    .option('-f, --function', 'Create a functional component')
+    .option('-c, --class', 'Create a class based component')
+    .option('-s, --state', "Add state to the component")
+    .option('--css', `Add <component-name>.css`)
+    .parse(process.argv);
+
+createComponent(componentName);
+
+function createComponent(componentName) {
+    if (componentName === undefined) {
+        console.error('ERROR: No component name specified!');
+        process.exit(1);
+    }
+
+    const root = process.cwd() + '\\src\\' + componentName.replace(/\//g,'\\');
+    componentName = root.split('\\').pop();
+    let className = componentName.charAt(0).toUpperCase() + componentName.slice(1);
+
+    if(!fs.existsSync(root)) {
+        fs.mkdirSync(root, {recursive: true});
+    }
+
+    if(program.css) {
+        fs.writeFileSync(
+            path.join(root, `${componentName}.css`),
+            `/*\nCSS Module: ${componentName}\n*/\n`+
+            `.${className} {\n`+
+            '\t/* Add css styles for your component here */\n'+
+            '}'
+        )
+        console.log(`CREATED ${root}\\${componentName}.css`)
+    }
+
+    if(program.function) {
+        fs.writeFileSync(
+            path.join(root, `${componentName}.js`),
+            `import React, {useState} from 'react';\n`+
+            (program.css ? `import classes from './${componentName}.css';\n\n` : '\n')+
+            `const ${componentName} = (props) => {\n`+
+            (program.state ? '\tconst [stateVar, setStateVar] = useState(0);\n': '')+
+            '\treturn(\n'+
+            '\t\t<div ' + (program.css ? `className={classes.${className}}` : '') + '>\n\n\t\t</div>\n'+
+            '\t);\n'+
+            '}\n\n'+
+            `export default ${componentName};`
+        )
+    } else if(program.class) {
+        fs.writeFileSync(
+            path.join(root, `${componentName}.js`),
+            `import React, {Component} from 'react';\n`+
+            (program.css ? `import classes from './${componentName}.css';\n\n` : '\n')+
+            `class ${className} extends Component {\n`+
+            (program.state ? '\tconstructor(props) {\n'+
+            '\t\tsuper(props)\n'+
+            '\t\tthis.state = { /*Add your state here */ }'+
+            '\t}\n': '')+
+            '\trender(\n'+
+            '\t\treturn(\n'+
+            '\t\t\t<div' + (program.css ? `className={classes.${className}}` : '') + '>\n\n\t\t\t</div>\n'+
+            '\t\t);\n'+
+            '\t)\n'+
+            '}\n\n'+
+            `export default ${className};`
+        )
+    }
+
+    if(program.function || program.class) {
+        console.log(`CREATED ${root}\\${componentName}.js`)
+        fs.writeFileSync(
+            path.join(root, 'index.js'),
+            `export { default } from './${componentName}';`
+        )
+        console.log(`CREATED ${root}\\index.js`)
+    } else {
+        console.error('ERROR: No option specified!');
+    }
+}
